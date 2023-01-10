ae84450d3a37e9a7c3e40811eead607919b012b8
Yves <vyves246@gmail.com>
2023-01-10 14:33:43 -0800

    Visiulzer done

--- Code/jan_10.js
+++ Code/jan_10.js
@@ -1,19 +1,39 @@
-const button1 = document.getElementById('button1');
-const audioCtx = new AudioContext();
-console.log(audioCtx);
+const contanier = document.getElementById('container');
+const canvas = document.getElementById('canvas1');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
+let audioSource;
+let analyser;
 
+contanier.addEventListener('click', function(){
+    const audio1 = document.getElementById('audio1');
+    // audio1.src
+    const audioCtx = new AudioContext();
+    audio1.play();
+    audioSource = audioCtx.createMediaElementSource(audio1);
+    analyser = audioCtx.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioCtx.destination)
+    analyser.fftSize = 64;
+    const bufferLength = analyser.frequencyBinCount; // half of fftSize
+    const dataArray = new Uint8Array(bufferLength); // convert 
 
+    const barWidth = canvas.width / bufferLength;
+    let barHeight;
+    let x;
 
-
-const button2 = document.getElementById('button2');
-button2.addEventListener('click', playSound);
-
-function playSound() {
-    const oscillator = audioCtx.createOscillator();
-    oscillator.connect(audioCtx.destination); // connect to speaker
-    oscillator.type = 'sine'; // sine, square, sawtooth, triangle
-    oscillator.start();
-    setTimeout(function(){
-        oscillator.stop();
-    }, 200);
-}
+    function animate() {
+        x=0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        for(let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i]; // adjust for height of bar
+            ctx.fillStyle = 'white';
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+            x += barWidth;
+        }
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
