2f4a9421f35e248538664e418d5e8498b58fa4c9
Yves Velasquez <83028938+HallowsYves@users.noreply.github.com>
2023-01-21 15:40:34 -0800

    Persian Rug

--- Code/Jan_22.js
+++ Code/Jan_22.js
@@ -1,1 +1,33 @@
+f = 50;  //font size
+col = ["lightblue","beige","lightyellow","black","gold"]
 
+function setup() {
+  createCanvas(700, 700);
+  background(0);
+  textSize(f);
+  for(x=0;x<width+f;x+=f*0.3){
+    for (y=0;y<height+f;y+=f*1.4){
+      c=random(3);
+      stroke(col[floor(random(5))])
+      fill(col[floor(random(5))])
+      if (c<1){
+        text("░",y,x)
+      }
+      else if (c<2){
+        text("▒",x,y)
+      }
+     else if (c<3){
+        text("▓",x,y)
+      }
+      else if (c<4){
+        text("█",x,y)
+      }
+    }
+  }  
+}
+
+function keyTyped() {
+  if (key === "s") {
+    save("Jan_21.jpg");
+  }
+}
